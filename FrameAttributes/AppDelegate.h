959b0b7e5e891acad706cdefadfeac99b2f77650
Rui Cao <rayray199085@gmail.com>
2019-02-03 10:12:51 +1100

    Initial Commit

---
+++ FrameAttributes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FrameAttributes
+//
+//  Created by Stephen Cao on 3/2/19.
+//  Copyright © 2019 Stephen Cao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FrameAttributes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FrameAttributes
+//
+//  Created by Stephen Cao on 3/2/19.
+//  Copyright © 2019 Stephen Cao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
