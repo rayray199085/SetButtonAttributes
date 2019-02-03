6956e9807ede9d18535a4395febf5ec91bc2e9c6
Rui Cao <rayray199085@gmail.com>
2019-02-03 17:26:58 +1100

    first commit

--- FrameAttributes/ViewController.m
+++ FrameAttributes/ViewController.m
@@ -7,16 +7,85 @@
 //
 
 #import "ViewController.h"
-
+typedef CGPoint (^imageTranslateBlock)(CGPoint point);
+typedef CGRect (^imageScaleBlock)(CGRect rect);
 @interface ViewController ()
-
+-(IBAction)up;
+-(IBAction)down;
+-(IBAction)right;
+-(IBAction)left;
+-(IBAction)plus;
+-(IBAction)minus;
+-(void)translateImage:(imageTranslateBlock) block;
+-(void)scaleImage:(imageScaleBlock) block;
+@property(nonatomic,weak)IBOutlet UIButton * mImageButton;
 @end
 
 @implementation ViewController
-
+-(void)scaleImage:(imageScaleBlock) block{
+//    [UIView beginAnimations:nil context:nil];
+//    [UIView setAnimationDuration:1.0];
+    [UIView animateWithDuration:1.0 animations:^{
+        CGRect tempRect = self.mImageButton.bounds;
+        self.mImageButton.bounds = block(tempRect);;
+    }];
+//    [UIView commitAnimations];
+}
+-(void)translateImage:(imageTranslateBlock) block{
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationDuration:1.0];
+    CGPoint tempPoint = self.mImageButton.center;
+    self.mImageButton.center = block(tempPoint);;
+    [UIView commitAnimations];
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+-(IBAction)up{
+//    NSLog(@"up");
+    [self translateImage:^CGPoint(CGPoint point) {
+        point.y -= 10;
+        return point;
+    }];
+}
+-(IBAction)down{
+//    NSLog(@"down");
+    [self translateImage:^CGPoint(CGPoint point) {
+        point.y += 10;
+        return point;
+    }];
+}
+-(IBAction)right{
+//    NSLog(@"right");
+    [self translateImage:^CGPoint(CGPoint point) {
+        point.x += 10;
+        return point;
+    }];
+}
+-(IBAction)left{
+//    NSLog(@"left");
+    [self translateImage:^CGPoint(CGPoint point) {
+        point.x -= 10;
+        return point;
+    }];
+}
+-(IBAction)plus{
+//    NSLog(@"plus");
+    [self scaleImage:^CGRect(CGRect rect) {
+        rect.size.height += 10;
+        rect.size.width += 10;
+        return rect;
+    }];
+}
+-(IBAction)minus{
+//    NSLog(@"minus");
+    [self scaleImage:^CGRect(CGRect rect) {
+        rect.size.height -= 10;
+        rect.size.width -= 10;
+        return rect;
+    }];
+    
 }
 
 
